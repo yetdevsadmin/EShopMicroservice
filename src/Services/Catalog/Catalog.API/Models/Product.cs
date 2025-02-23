28ff9700c6855ea0bee29631b1f028ce53079af3
jrakesh <JRakesh@korewireless.com>
2025-02-23 23:48:34 +0530

    second commit

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new ();
+        public string Description { get; set; } = default!;
+        public string ImageFil { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
