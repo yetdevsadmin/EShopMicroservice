0d2c5ea6f5f83357d12321fefcdc221be75f16d4
jrakesh <JRakesh@korewireless.com>
2025-02-26 13:04:14 +0530

    commit

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; } = default!;
         public List<string> Category { get; set; } = new ();
         public string Description { get; set; } = default!;
-        public string ImageFil { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
         public decimal Price { get; set; }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,20 +1,36 @@
-﻿using MediatR;
-
+﻿
 namespace Catalog.API.Products.CreateProduct
 {
 
     
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Business logic to create a product.
-            throw new NotImplementedException();
+            //create Product entity from command object
+            
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+
+            //return result
+            return new CreateProductResult(product.Id);
         }
     }
 }
