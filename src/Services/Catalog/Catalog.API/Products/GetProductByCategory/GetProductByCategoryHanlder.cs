6f849ce203d86979bd5abc8141790269c55d63ed
rakeshjose <rakeshjose84@outlook.com>
2025-02-27 13:37:51 +0530

    new endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHanlder.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductByCategoryQueryHanlder(IDocumentSession session, ILogger<GetProductByCategoryQueryHanlder> logger) 
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHanlder.Handle called with {@query}", query);
+
+            var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdQueryHandler (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger):
+         IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {query}", query);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product == null)
+            {
+                throw new ProductNotFoundException(query.Id);
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
