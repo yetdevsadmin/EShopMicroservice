0c4ee0f4ce9e3fc2cbc947f0974abfef8019153a
rakeshjose <rakeshjose84@outlook.com>
2025-03-13 23:17:53 +0530

    ordering API

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+           
+            return app;
+        }
+    }
+}
